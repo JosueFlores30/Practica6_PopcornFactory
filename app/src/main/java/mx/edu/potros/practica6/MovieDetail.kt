c1a424e60db6de182f32714a62c093e30f0ef93d
JosueFlores30 <josue.flores228704@potros.itson.edu.mx>
2023-02-28 22:52:08 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/potros/practica6/MovieDetail.kt
@@ -0,0 +1,33 @@
+package mx.edu.potros.practica6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class MovieDetail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+        cargarDescripcion()
+    }
+
+    fun cargarDescripcion(){
+
+        var title: TextView = findViewById(R.id.tituloPelicula) as TextView
+        var description: TextView = findViewById(R.id.descripcionPelicula) as TextView
+        var image: ImageView = findViewById(R.id.imagenPelicula) as ImageView
+
+        intent.getStringExtra("titulo")
+
+        var tituloPelicula= intent.getStringExtra("titulo")
+        var descripcionPelicula= intent.getStringExtra("sinopsis")
+        var imagenPelicula= intent.getIntExtra("header", 0)
+
+        title.setText(tituloPelicula)
+        description.setText(descripcionPelicula)
+        image.setImageResource(imagenPelicula)
+    }
+
+}
